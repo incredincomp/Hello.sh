0de3c912fc1d1b29502bb1ab1dc1d516405da499
incredincomp <incredincomp@gmail.com>
2018-10-18 13:05:36 -0700

    Update Hello.sh

--- Hello.sh
+++ Hello.sh
@@ -3,4 +3,9 @@ echo "Hello! Whats your name?"
 read playername
 echo "Hey $playername! How are you doing today?
 read mood
-case mood
+if ( $mood = "fantastic")
+echo "Thats great $playername! Everyone should always feel fantastic!"
+else
+echo "oh no $playername.. $mood is not a great way to be. You should be fantastic!"
+fi
+
